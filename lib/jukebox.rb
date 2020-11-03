0043793c94163700a2a2d3d4bb9febd5affcff34
Alexander Bowers <7319abowers@gmail.com>
2020-11-03 07:19:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ current_song = ""
 puts "Please enter a song name or number:"
 song_choice = gets.strip
 songs.any? do |song_name|
-  if song_name == song_choice 
+  if song_name == song_choice
     current_song = "Playing #{song_name}"
 end
 
