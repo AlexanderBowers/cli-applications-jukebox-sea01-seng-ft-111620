ac7558444f84375dc69e68f4e06f4e82eb525405
Alexander Bowers <7319abowers@gmail.com>
2020-11-03 07:09:13 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   song_choice = gets.strip
   songs.each do |song_name|
-    binding.pry
+    #binding.pry
   current_song = "Playing #{songs[song_choice.to_i - 1]}"
     end
   end
