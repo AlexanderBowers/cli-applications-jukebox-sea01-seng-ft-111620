af2e168154a1d6ef63a92d1cb251fcb7805df311
Alexander Bowers <7319abowers@gmail.com>
2020-11-03 07:36:36 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,10 +30,9 @@ def play(songs)
   puts "Please enter a song name or number:"
   song_choice = gets.strip
   songs.each do |song_name|
-    until song_name == song_choice || (song_choice.to_i - 1) == songs.index(song_name)
-      puts "Invalid input, please try again"
-    end
+    if song_name == song_choice || (song_choice.to_i - 1) == songs.index(song_name)
       puts  "Playing #{song_name}"
+    end
   end
 end
 
